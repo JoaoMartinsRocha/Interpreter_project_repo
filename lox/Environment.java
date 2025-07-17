0dc4f86bfc32f9ce060ade8a2bc8f42dc8c67335
João Pedro Rocha <124220623+JoaoMartinsRocha@users.noreply.github.com>
2025-07-17 12:03:19 -0300

    Implemented the resolver to fix the dynamic scoping issue

--- lox/Environment.java
+++ lox/Environment.java
@@ -19,12 +19,28 @@ class Environment {
         values.put(name, value);
     }
 
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
 
-        // Check enclosing environments in case of no value in this one 
+        // Check enclosing environments in case of no value in this one (This shouldn't be needed after implementing the resolver)
         if (enclosing != null) {
             return enclosing.get(name);
         }
